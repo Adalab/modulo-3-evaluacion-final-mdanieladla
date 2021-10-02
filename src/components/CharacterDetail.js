d3514b8a88a168f0846f07e983cfe00fb1f7117e
mdanieladla <mariadanieladarnea@gmail.com>
2021-10-02 18:20:53 +0200

    add details characters

---
+++ src/components/CharacterDetail.js
@@ -0,0 +1,15 @@
+const CharacterDetail = (props) => {
+  console.log('CharacterDetail props=', props);
+  return (
+    <div className='container-detail'>
+      {/* <img src={data.image} alt='img' />
+      <h3>{data.name}</h3>
+      <p>Status: {data.status}</p>
+      <p>Species: {data.species}</p>
+      <p>Origin: {data.origin}</p>
+      <p>Episode: {data.episode}</p> */}
+    </div>
+  );
+};
+
+export default CharacterDetail;
