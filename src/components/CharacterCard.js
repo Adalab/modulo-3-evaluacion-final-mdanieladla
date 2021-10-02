6b8e3d7231cb003a07655654ae1ac95e8bee9f5d
mdanieladla <mariadanieladarnea@gmail.com>
2021-10-02 18:20:33 +0200

    fixed problem

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,13 +1,14 @@
 const CharacterCard = (props) => {
+  //console.log('CharacterCard', props);
   return (
     <>
       <img
         className='container__info--img'
-        src={props.character.image}
-        alt={`Img of ${props.character.name}`}
+        src={props.characterData.image}
+        alt={`Img of ${props.characterData.name}`}
       />
-      <h3 className='container__info--name'>{props.character.name}</h3>
-      <h4 className='container__info--specie'>{props.character.species}</h4>
+      <h3 className='container__info--name'>{props.characterData.name}</h3>
+      <h4 className='container__info--specie'>{props.characterData.species}</h4>
     </>
   );
 };
