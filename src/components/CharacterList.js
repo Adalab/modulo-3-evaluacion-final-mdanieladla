bf40d7e860d5c32d6e6c2bd9c0620a610303e6ae
mdanieladla <mariadanieladarnea@gmail.com>
2021-10-04 13:47:42 +0200

    add comments

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,15 +1,16 @@
 import { Link } from 'react-router-dom';
+//Componentes
 import CharacterCard from './CharacterCard';
 import NotACharacter from './NotACharacter';
 
 const CharacterList = (props) => {
-  console.log('characterlist=', props.data);
+  //console.log('characterlist=', props.data);
   if (props.data.length === 0) {
     return <NotACharacter />;
   } else {
     const htmlList = props.data.map((characterData, i) => (
       <li className='container__info' key={characterData.id}>
-        <Link to={`/character/${characterData.id}`}>
+        <Link to={`/character/${characterData.id}`} className='link'>
           <CharacterCard characterData={characterData} />
         </Link>
       </li>
