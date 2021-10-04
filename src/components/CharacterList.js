9b968357ec1ba8920ca04a1b2e66e2d7c1336501
mdanieladla <mariadanieladarnea@gmail.com>
2021-10-04 09:58:59 +0200

    fixed route

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom';
 import CharacterCard from './CharacterCard';
 
 const CharacterList = (props) => {
+  console.log('characterlist=', props);
   const htmlList = props.data.map((characterData, i) => (
     <li className='container__info' key={characterData.id}>
-      <CharacterCard characterData={characterData} />
+      <Link to={`/character/${characterData.id}`}>
+        <CharacterCard characterData={characterData} />
+      </Link>
     </li>
   ));
   return <ul className='container'>{htmlList}</ul>;
