495e6744a8281c0801d0b63dc8f691a35f1540e9
mdanieladla <mariadanieladarnea@gmail.com>
2021-10-04 12:09:00 +0200

    import notacharacter component

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,21 @@
 import { Link } from 'react-router-dom';
 import CharacterCard from './CharacterCard';
+import NotACharacter from './NotACharacter';
 
 const CharacterList = (props) => {
-  console.log('characterlist=', props);
-  const htmlList = props.data.map((characterData, i) => (
-    <li className='container__info' key={characterData.id}>
-      <Link to={`/character/${characterData.id}`}>
-        <CharacterCard characterData={characterData} />
-      </Link>
-    </li>
-  ));
-  return <ul className='container'>{htmlList}</ul>;
+  console.log('characterlist=', props.data);
+  if (props.data.length === 0) {
+    return <NotACharacter />;
+  } else {
+    const htmlList = props.data.map((characterData, i) => (
+      <li className='container__info' key={characterData.id}>
+        <Link to={`/character/${characterData.id}`}>
+          <CharacterCard characterData={characterData} />
+        </Link>
+      </li>
+    ));
+    return <ul className='container'>{htmlList}</ul>;
+  }
 };
 
 export default CharacterList;
