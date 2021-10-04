8359d53874e21187bf87940d387d17ab15127f57
mdanieladla <mariadanieladarnea@gmail.com>
2021-10-04 12:09:31 +0200

    create not found page component

---
+++ src/components/NotFoundPage.js
@@ -0,0 +1,7 @@
+import '../styles/layout/notFoundPage.scss';
+
+const NotFouondPage = () => {
+  return <p className='no-page'>La página que buscas no existe, sorry.</p>;
+};
+
+export default NotFouondPage;
