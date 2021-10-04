1ae2466d18ddf02f2a01e86f379261cc66b5138c
mdanieladla <mariadanieladarnea@gmail.com>
2021-10-04 12:09:17 +0200

    create not a character component

---
+++ src/components/NotACharacter.js
@@ -0,0 +1,9 @@
+const NotACharacter = () => {
+  return (
+    <p className='no-page'>
+      El personaje que buscas no existe en esta galaxia, sorry.
+    </p>
+  );
+};
+
+export default NotACharacter;
