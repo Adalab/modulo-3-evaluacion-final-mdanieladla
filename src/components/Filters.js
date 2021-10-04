fe5b5802282e01ff33ca86648b8aeeea62b1f268
mdanieladla <mariadanieladarnea@gmail.com>
2021-10-04 09:23:11 +0200

    add label and new style

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,8 +1,15 @@
+import '../styles/layout/filter.scss';
+
 const FilterCharacterByName = (props) => {
   return (
-    <form>
+    <form className='container__form'>
+      <label className='container__form--label'>
+        Busca por el nombre del personaje
+      </label>
       <input
+        className='container__form--input'
         type='text'
+        placeholder='Morty'
         value={props.search}
         onChange={props.handleChangeSearch}
       />
