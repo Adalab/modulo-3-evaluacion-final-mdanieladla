ce8ee16272027ba80222d4c0d83b4947e48bf25d
mdanieladla <mariadanieladarnea@gmail.com>
2021-10-04 13:49:05 +0200

    add style and img

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,7 +1,20 @@
+import { Link } from 'react-router-dom';
 import '../styles/layout/notFoundPage.scss';
-
+import portal from '../images/rick-and-morty-portal.png';
 const NotFouondPage = () => {
-  return <p className='no-page'>La página que buscas no existe, sorry.</p>;
+  return (
+    <div className='page'>
+      <Link to='/' className='link'>
+        <img
+          src={portal}
+          alt='Portal de vuelta a inicio'
+          className='portal-img2'
+          title='Volver a incio'
+        />
+      </Link>
+      <p className='no-page--par2'>La galaxia que buscas no existe, sorry.</p>
+    </div>
+  );
 };
 
 export default NotFouondPage;
